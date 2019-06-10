cddb2b5b44c0eaa4978d52312e15e3a9f468789a
安仲辉 <anzhonghui@topscomm.com>
2019-06-10 20:19:37 +0800

    cors解决跨域问题

---
+++ src/main/java/com/hx/imageserver/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.hx.imageserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author: AN
+ * @Description: 跨域问题
+ * @Date: Created in 19:57 2019/6/10
+ * @Modified by:
+ */
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        // 可以自行筛选
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
