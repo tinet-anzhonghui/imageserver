925289891c87009466b8948e2d1890a40f32e13e
安仲辉 <anzhonghui@topscomm.com>
2019-06-11 10:31:27 +0800

    cors解决服务器端跨域访问问题（response问题）

--- src/main/java/com/hx/imageserver/config/WebMvcConfiguration.java
+++ src/main/java/com/hx/imageserver/config/WebMvcConfiguration.java
@@ -2,23 +2,38 @@ package com.hx.imageserver.config;
 
 import com.hx.imageserver.controller.ImageServerController;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @Author: AN
- * @Description: 配置图片的位置
+ * @Description: 映射配置
  * @Date: Created in 16:08 2019/6/10
  * @Modified by:
  */
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    /**
+     * 配置图片的位置
+     * @param registry
+     */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //配置静态资源处理
         registry.addResourceHandler("/showImage/**")
                 .addResourceLocations("file:"+ImageServerController.BASE_DIR.toString()+"\\");
         super.addResourceHandlers(registry);
+    }
+
+
+    /**
+     * 跨域访问配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*").allowedHeaders("*");
     }
 }
